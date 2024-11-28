70903a6045e6ce088e73f975ba69b42e5077c25c
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-11-29 00:06:03 +0900

    feat: 로또 당첨 번호 검증

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -2,17 +2,20 @@ package lotto.controller
 
 import lotto.controller.domain.UserInteractionController
 import lotto.controller.validator.PurchaseCostValidator
+import lotto.controller.validator.LottoNumbersValidator
+import lotto.model.Lotto
 import lotto.model.MyLotto
 
 class LottoController(
     private val userInteractionController: UserInteractionController = UserInteractionController(),
     private val purchaseCostValidator: PurchaseCostValidator = PurchaseCostValidator(),
+    private val lottoNumbersValidator: LottoNumbersValidator = LottoNumbersValidator(),
 ) {
     fun run() {
         val purchaseCost = getPurchaseCost()
         val myLotto = MyLotto(purchaseCost)
         showPurchaseLotto(myLotto)
-        val lottoNumbers = getLottoNumber()
+        val winningNumbers = getLottoNumber()
     }
 
     private fun getPurchaseCost(): Int {
@@ -25,8 +28,10 @@ class LottoController(
         userInteractionController.handlePurchaseLotto(myLotto)
     }
 
-    private fun getLottoNumber(): String {
+    private fun getLottoNumber(): Lotto {
         val lottoNumbers = userInteractionController.handleLottoNumbers()
-        return lottoNumbers
+        lottoNumbersValidator(lottoNumbers)
+        val winningNumbers = Lotto(lottoNumbers.split(",").map { it.trim().toInt() })
+        return winningNumbers
     }
 }
