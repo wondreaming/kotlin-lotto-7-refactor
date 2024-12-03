17ef837988b945f4ebaa44d774a56cc2cc9c00e5
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-12-03 21:55:02 +0900

    refactor: 상수화

--- src/main/kotlin/lotto/util/RandomNumbers.kt
+++ src/main/kotlin/lotto/util/RandomNumbers.kt
@@ -4,6 +4,12 @@ import camp.nextstep.edu.missionutils.Randoms
 
 class RandomNumbers {
     operator fun invoke(): List<Int> {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
+        return Randoms.pickUniqueNumbersInRange(START_NUMBER, LAST_NUMBER, LOTTE_NUMBER_COUNT).sorted()
+    }
+
+    companion object {
+        private const val START_NUMBER = 1
+        private const val LAST_NUMBER = 45
+        private const val LOTTE_NUMBER_COUNT = 6
     }
 }
