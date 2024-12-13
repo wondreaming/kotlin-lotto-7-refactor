1d40b498f82dea39100474991ea252907b9316af
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-12-13 21:22:35 +0900

    refactor: 변수명 변경

--- src/main/kotlin/lotto/controller/validator/LottoNumbersValidator.kt
+++ src/main/kotlin/lotto/controller/validator/LottoNumbersValidator.kt
@@ -3,38 +3,38 @@ package lotto.controller.validator
 class LottoNumbersValidator {
     operator fun invoke(input: String) {
         isEmpty(input)
-        val nums = input.split(",").map { it.trim() }
-        isEmpty(nums)
-        isInteger(nums)
-        isBetween(nums)
-        isDuplicate(nums)
-        isSix(nums)
+        val numbers = input.split(",").map { it.trim() }
+        isEmpty(numbers)
+        isInteger(numbers)
+        isBetween(numbers)
+        isDuplicate(numbers)
+        isSix(numbers)
     }
 
     private fun isEmpty(input: String) {
         require(input.isNotEmpty()) { LottoNumbersErrorType.EMPTY_INPUT }
     }
 
-    private fun isEmpty(nums: List<String>) {
-        require(nums.all { it.isNotEmpty() }) { LottoNumbersErrorType.EMPTY_INPUT }
+    private fun isEmpty(numbers: List<String>) {
+        require(numbers.all { it.isNotEmpty() }) { LottoNumbersErrorType.EMPTY_INPUT }
     }
 
-    private fun isInteger(nums: List<String>) {
-        require(nums.all { it.toIntOrNull() != null }) { LottoNumbersErrorType.NOT_INTEGER }
+    private fun isInteger(numbers: List<String>) {
+        require(numbers.all { it.toIntOrNull() != null }) { LottoNumbersErrorType.NOT_INTEGER }
     }
 
-    private fun isBetween(nums: List<String>) {
-        val nums = nums.map { it.toInt() }
+    private fun isBetween(numbers: List<String>) {
+        val nums = numbers.map { it.toInt() }
         require(nums.all { it in 1..45 }) { LottoNumbersErrorType.BETWEEN_1_45 }
     }
 
-    private fun isDuplicate(nums: List<String>) {
-        val nums = nums.map { it.toInt() }
+    private fun isDuplicate(numbers: List<String>) {
+        val nums = numbers.map { it.toInt() }
         require(nums.size == nums.toSet().size) { LottoNumbersErrorType.DUPLICATE }
     }
 
-    private fun isSix(nums: List<String>) {
-        val nums = nums.map { it.toInt() }
+    private fun isSix(numbers: List<String>) {
+        val nums = numbers.map { it.toInt() }
         require(nums.size == 6) { LottoNumbersErrorType.SIX }
     }
 }
